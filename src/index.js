81b24601599bf038dbd89a66cb3d458e1daf7d38
lupevillena <guadalupevillena.h@gmail.com>
2026-09-09 00:20:29 -0600

    Limpia logs temporales de Railway

--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_PORT:', process.env.DB_PORT);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_NAME:', process.env.DB_NAME);
-
 app.listen(PORT, () => {
   console.log(`Servidor funcionando en puerto ${PORT}`);
 });
--- src/pool.js
+++ src/pool.js
@@ -2,19 +2,12 @@ require('dotenv').config();
 
 const { Pool } = require('pg');
 
-const config = {
+const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT)
-};
-
-console.log('POOL HOST:', config.host);
-console.log('POOL PORT:', config.port);
-console.log('POOL USER:', config.user);
-console.log('POOL DATABASE:', config.database);
-
-const pool = new Pool(config);
+});
 
 module.exports = pool;
